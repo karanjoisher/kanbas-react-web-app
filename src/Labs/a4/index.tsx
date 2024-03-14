dc4cc8431c762bc83b3dd60d4ca0c16dcd1fa595
Karan Joisher <karanjoisher@gmail.com>
2024-03-14 13:15:20 -0400

    2.3

--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -3,6 +3,13 @@ import ClickEvent from "./ClickEvent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
 import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
 const Assignment4 = () => {
   function sayHello() {
     alert("Hello");
@@ -15,6 +22,13 @@ const Assignment4 = () => {
       <PassingDataOnEvent />
       <PassingFunctions theFunction={sayHello}/>
       <EventObject/>
+      <Counter />
+      <BooleanStateVariables />
+      <StringStateVariables />
+      <DateStateVariable />
+      <ObjectStateVariable />
+      <ArrayStateVariable />
+      <ParentStateComponent />
     </>
   );
 };
