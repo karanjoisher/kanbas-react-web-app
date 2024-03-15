59d7e251917d48a84db3aafcebaa65e8ab521d14
Karan Joisher <karanjoisher@gmail.com>
2024-03-15 19:09:05 -0400

    Completed 3.4.4 and the entire assignment!

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -3,13 +3,22 @@ import { FaCheckCircle, FaClipboard, FaEllipsisV, FaPlus, FaPlusCircle } from "r
 import { Link, useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { KanbasState } from "../../store";
+import { deleteAssignment } from "./assignmentsReducer";
+
+
 
 
 function Assignments() {
     const { courseId } = useParams();
+    const dispatch = useDispatch();
     const assignments = useSelector((state: KanbasState) =>
         state.assignmentsReducer.assignments);
-    
+    function handleDelete(id: any) {
+        const confirmed = window.confirm("Are you sure you want to delete this assignment?");
+        if (confirmed) dispatch(deleteAssignment(id));
+    }
+
+
     return (
         <>
             <div className="top-row">
@@ -18,7 +27,7 @@ function Assignments() {
                     <button className="border-1 border-light p-2 me-2"><FaPlus className="me-1" />Group</button>
                     <Link
                         to={`/Kanbas/Courses/${courseId}/Assignments/new`}><button className="border-1 border-light p-2 me-2 text-light bg-danger"><FaPlus className="me-1" />Assignment</button></Link>
-                    
+
                     <button className="border-1 border-light p-2 me-2" onClick={() => window.location.href = '/Kanbas/Courses/Assignments/Edit/screen.html'}><FaEllipsisV className="mb-1" /></button>
                 </span>
             </div>
@@ -41,6 +50,7 @@ function Assignments() {
                                     <Link
                                         to={`/Kanbas/Courses/${courseId}/Assignments/${assignment._id}`}>{assignment.title}</Link>
                                     <span className="float-end">
+                                        <button className="btn btn-danger mx-2 rounded px-2" onClick={() => { handleDelete(assignment._id) }}>Delete</button>
                                         <FaCheckCircle className="text-success" /><FaEllipsisV className="ms-2" /></span>
                                 </li>))}
                     </ul>
